75bec27fdf63bea5ad3013b2610b5e80740bc622
SEUNGHO1216 <seungho1216@naver.com>
2025-01-11 01:53:49 +0900

    feat: configuration edit -> application.yml with the content of DB info

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
   implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
   kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
-  runtimeOnly("com.h2database:h2")
   runtimeOnly("com.mysql:mysql-connector-j")
 
   testImplementation("com.h2database:h2")
